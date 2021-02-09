9560834da0fd802dbafc3d485b65a3889201ee2e
Johnathan Mitri <600018217@fjuhsd.org>
2021-02-09 03:30:10 +0000

    wrote arrayListFunHouse methods

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -10,12 +10,36 @@ import static java.lang.System.*;
 
 public class ArrayListFunHouse
 {
- public static ArrayList<Integer> getListOfFactors(int number)
- {
-  return null;
- }
  
- public static void keepOnlyCompositeNumbers( List<Integer> nums )
- {
- }
+  public static ArrayList<Integer> getListOfFactors(int number)
+  {                                         
+	  //declare and initialize a new empty arraylist of integers
+    ArrayList<Integer> factors = new ArrayList<Integer>();
+     	//for loop that starts from 2, and goes up to the number
+    for (int i = 2; i < number; i ++)
+	  {
+      //if the number is divisible by the current number
+      if (number % i == 0)
+		  {
+			  //add the current number to the arrayList
+        factors.add(i);
+		  }
+	  }
+	  //return the arraylist
+    return factors;
+  }
+ 
+  public static void keepOnlyCompositeNumbers( List<Integer> nums )
+  {
+	  //for loop that loops backwards through the ArrayList
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+      //if the current number is not composite
+      if (getListOfFactors(nums.get(i)).size() < 1)
+      {
+        //remove the current element from the list
+        nums.remove(i);
+      }
+    }
+  }
 }
